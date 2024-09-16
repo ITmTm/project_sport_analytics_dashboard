5f373c3564360fb0acf15adf4f472d50253b5c33
Timur <Stafr95@mail.ru>
2024-09-16 15:26:41 +0500

    Connecting store and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
+
 import './styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <Provider store={store}>
+          <App />
+      </Provider>
+  </StrictMode>
+);
