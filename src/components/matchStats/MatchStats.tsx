2cbb919c2a3f4aaed7074afc2b5d050deeda0d31
Timur <Stafr95@mail.ru>
2024-09-25 19:03:00 +0500

    adding data validation

--- src/components/matchStats/MatchStats.tsx
+++ src/components/matchStats/MatchStats.tsx
@@ -9,32 +9,38 @@ interface MatchStateProps {
 }
 
 const MatchStats: React.FC<MatchStateProps> = ({ stats }) => {
-    const [chartData, setChartData] = useState({ labels: [], datasets: [] });
+    const [chartData, setChartData] = useState({
+        labels: [], datasets: []
+    });
 
     useEffect(() => {
-        const labels = Object.keys(stats);
-        const data = Object.values(stats);
+        if (Object.keys(stats).length > 0) {        // Проверка на наличие данных
+            const labels = Object.keys(stats);
+            const data = Object.values(stats);
 
-        setChartData({
-            labels,
-            datasets: [
-                {
-                    label: 'Статистика по матчам',
-                    data,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, .6)',
-                        'rgba(54, 162, 235, .6)',
-                        'rgba(255, 206, 86, .6)',
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                    ],
-                    borderWidth: 1,
-                },
-            ],
-        });
+
+            setChartData({
+                labels,
+                datasets: [
+                    {
+                        label: 'Статистика по матчам',
+                        data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, .6)',
+                            'rgba(54, 162, 235, .6)',
+                            'rgba(255, 206, 86, .6)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            });
+
+        }
 
     }, [stats]);
 
